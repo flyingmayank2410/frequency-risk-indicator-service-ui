045dcb8c3e41b1bdf2f7f9b20669adf0d67c00a5
z004ue7v <mayank-gupta@siemens.com>
2025-08-01 06:24:13 +0530

    updated file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   root: '.', // Make sure root is set correctly
   publicDir: 'public', // This is the default, but you can be explicit
