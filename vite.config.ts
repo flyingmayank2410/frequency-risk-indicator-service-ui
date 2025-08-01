8e28dedf6e10c41d9aee2555667a1e12174513b1
z004ue7v <mayank-gupta@siemens.com>
2025-08-01 06:28:50 +0530

    updated file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  root: '.', // Make sure root is set correctly
-  publicDir: 'public', // This is the default, but you can be explicit
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './public/index.html', // 👈 This line is important
+      input: './index.html', // ✅ Correct input
     },
   },
 });
