1642cb37ecbf1ac0263595357b544b4603585ba1
z004ue7v <mayank-gupta@siemens.com>
2025-08-01 06:12:55 +0530

    updated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-// src/App.tsx
 import React, { useEffect, useState } from "react";
 import TreeView from "./components/TreeView";
 import GraphPanel from "./components/GraphPanel";
 import LocationForm from "./components/LocationForm";
 import SwitchgearForm from "./components/SwitchgearForm";
-import { getAllLocations, getSwitchgearsByLocation } from "./api";
+import { getLocations, getSwitchgears } from "./api";
 
 interface Location {
   id: number;
@@ -14,28 +13,61 @@ interface Location {
 interface Switchgear {
   swgId: number;
   swgName: string;
+  locationId: number;
 }
 
-const App: React.FC = () => {
+function App() {
   const [locations, setLocations] = useState<Location[]>([]);
-  const [switchgears, setSwitchgears] = useState<Record<number, Switchgear[]>>({});
+  const [switchgears, setSwitchgears] = useState<Switchgear[]>([]);
   const [selectedLocationId, setSelectedLocationId] = useState<number | null>(null);
 
   const fetchData = async () => {
-    const locRes = await getAllLocations();
-    const locs = locRes?.data?.data || [];
-    setLocations(locs);
+    const locRes = await getLocations();
+    if (locRes?.data?.data) {
+      setLocations(locRes.data.data);
+    }
 
-    const swgMap: Record<number, Switchgear[]> = {};
-    await Promise.all(
-      locs.map(async (loc: any) => {
-        const swgRes = await getSwitchgearsByLocation(loc.id);
-        swgMap[loc.id] = swgRes?.data?.data || [];
-      })
+    const swgRes = await Promise.all(
+      locRes.data.data.map((loc: Location) => getSwitchgears(loc.id))
     );
-    setSwitchgears(swgMap);
+
+    const allSwitchgears = swgRes
+      .flatMap((res) => res?.data?.data || [])
+      .map((sg) => ({
+        ...sg,
+        swgId: sg.swgId,
+        swgName: sg.swgName,
+        locationId: sg.locationId,
+      }));
+
+    setSwitchgears(allSwitchgears);
   };
 
   useEffect(() => {
     fetchData();
-  }
+  }, []);
+
+  return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Frequency Risk UI</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-1">
+          <TreeView
+            locations={locations}
+            switchgears={switchgears}
+            onSelectLocation={setSelectedLocationId}
+          />
+        </div>
+        <div className="col-span-3">
+          <GraphPanel locationId={selectedLocationId ?? 0} />
+          <LocationForm onSuccess={fetchData} />
+          {selectedLocationId && (
+            <SwitchgearForm locationId={selectedLocationId} onSuccess={fetchData} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
